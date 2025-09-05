244305033d612f6e3db4ce26373cd43b160ec70e
Harrison <hmabramsohn@wm.edu>
2025-09-05 14:35:38 -0400

    Planning comments

---
+++ pages/Insurance2.py
@@ -0,0 +1,4 @@
+# This page will construct a U.S.-based map displaying property insurance loss ratio as a function of zip code.
+# Loss ratios will be displayed on a color scale. Loss rations of over >=100%, indicating a financial loss, will be uniform on a separate, striking color.
+# The map will be adjustable by year.
+# The map may be adjustable between displaying zip codes vs states. 
